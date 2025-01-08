2e2a8dd43524141e0b18b5e0a0131c6522ed615f
ygchbxm <1532074469@qq.com>
2025-01-09 00:25:35 +0800

    更新B站爬虫相关代码

--- B站爬虫/src/index.js
+++ B站爬虫/src/index.js
@@ -32,94 +32,7 @@ async function keywordSearch(keyWord) {
 }
 
 
-const BVId = 'BV1GU4y1p7j1';
-
-// BVIdInfo(BVId)
-
-async function BVIdInfo(BVId) {
-    const url = `https://www.bilibili.com/video/BV1Htr8YhELV/?spm_id_from=333.1007.tianma.1-1-1.click`
-    try {
-        const res = await axios({
-            method: 'get',
-            url: 'https://xy113x200x108x156xy2408y8770yy4y8000y27xy.mcdn.bilivideo.cn:4483/upgcxcode/55/91/422629155/422629155_nb2-1-30032.m4s?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M=&uipk=5&nbs=1&deadline=1736277951&gen=playurlv2&os=mcdn&oi=2004602649&trid=00000f6a722ee7694cba8318d886fca84530u&mid=0&platform=pc&og=cos&upsig=3e518d06271fb68f444717919dd0a263&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform,og&mcdnid=50016471&bvc=vod&nettype=0&orderid=0,3&buvid=38F47817-0310-AC32-92C8-4724739B6C6983763infoc&build=0&f=u_0_0&agrr=0&bw=60178&logo=A0020000',
-            headers: {
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-                "referer": "https://search.bilibili.com/all?vt=70366269&keyword=%E7%88%AC%E8%99%ABb%E7%AB%99%E8%A7%86%E9%A2%91&from_source=webtop_search&spm_id_from=333.788&search_source=5"
-            }
-        })
-        const dirPath = path.resolve(__dirname, '../saveFile');
-        await fs.writeFileSync(path.resolve(dirPath, 'test.mp4'), res.data);
-        console.log(res)
-    } catch (err) {
-        return err;
-    }
-}
-
-// audioInfo()
-
-async function audioInfo() {
-    const url = `https://www.bilibili.com/video/BV1GU4y1p7j1/?spm_id_from=333.1007.tianma.1-1-1.click`
-    try {
-        const res = await axios({
-            method: 'get',
-            url: 'https://upos-sz-estghw.bilivideo.com/upgcxcode/42/48/303114842/303114842-1-30280.m4s?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M=&uipk=5&nbs=1&deadline=1736279448&gen=playurlv2&os=upos&oi=2004602762&trid=8c3ffeba7f4b4d7a86a77d9a0f1b5144u&mid=0&platform=pc&og=hw&upsig=4108bed28a5f50bd372d8ae128f7936f&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform,og&bvc=vod&nettype=0&orderid=0,3&buvid=8E8842DA-1794-2021-FC7D-E3F603D56DBE38051infoc&build=0&f=u_0_0&agrr=0&bw=19736&logo=80000000',
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-                "Referer": url
-            },
-            responseType: 'arraybuffer',
-        })
-        const binaryData = Buffer.from(res.data, 'binary');
-        const dirPath = path.resolve(__dirname, '../saveFile');
-        await fs.writeFileSync(path.resolve(dirPath, 'test.mp3'), binaryData);
-        console.log(res)
-    } catch (err) {
-        console.log(err)
-        return err;
-    }
-}
-
-// VideosInfo();
-
-async function VideosInfo() {
-    const url = `https://www.bilibili.com/video/BV1GU4y1p7j1/?spm_id_from=333.1007.tianma.1-1-1.click`
-    try {
-        const res = await axios({
-            method: 'get',
-            url: 'https://upos-sz-estgoss.bilivideo.com/upgcxcode/42/48/303114842/303114842-1-100110.m4s?e=ig8euxZM2rNcNbdlhoNvNC8BqJIzNbfqXBvEqxTEto8BTrNvN0GvT90W5JZMkX_YN0MvXg8gNEV4NC8xNEV4N03eN0B5tZlqNxTEto8BTrNvNeZVuJ10Kj_g2UB02J0mN0B5tZlqNCNEto8BTrNvNC7MTX502C8f2jmMQJ6mqF2fka1mqx6gqj0eN0B599M=&uipk=5&nbs=1&deadline=1736279448&gen=playurlv2&os=upos&oi=2004602762&trid=8c3ffeba7f4b4d7a86a77d9a0f1b5144u&mid=0&platform=pc&og=cos&upsig=17a468d5d768caf07f0816d8b172e4f8&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform,og&bvc=vod&nettype=0&orderid=0,3&buvid=8E8842DA-1794-2021-FC7D-E3F603D56DBE38051infoc&build=0&f=u_0_0&agrr=0&bw=35454&logo=80000000',
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-                "Referer": url
-            },
-            responseType: 'arraybuffer',
-        })
-        const binaryData = Buffer.from(res.data, 'binary');
-        const dirPath = path.resolve(__dirname, '../saveFile');
-        await fs.writeFileSync(path.resolve(dirPath, 'test.mp4'), binaryData);
-        console.log(res)
-    } catch (err) {
-        console.log(err)
-        return err;
-    }
-}
-
-async function createDirectory(dirPath) {
-    fs.mkdir(dirPath, {recursive: true}, (err) => {
-        if (err) {
-            if (err.code === 'EEXIST') {
-                // 文件夹已存在
-                console.log(`The directory already exists: ${dirPath}`);
-            } else {
-                // 其他错误
-                console.error(`Error creating directory: ${err.message}`);
-            }
-        } else {
-            // 文件夹成功创建
-            console.log(`Directory created successfully: ${dirPath}`);
-        }
-    });
-}
-
+const BVId = 'BV1nF411C7Rm';
 
 const fileUrl = path.resolve(__dirname, `../saveFile/${BVId}.html`);
 const {JSDOM} = require('jsdom');
@@ -144,7 +57,7 @@ async function getBVInfo(BVId) {
     }
 }
 
-// playInfo(fileUrl)
+playInfo(fileUrl)
 async function playInfo(fileUrl) {
     const res = await fs.readFileSync(fileUrl, 'utf-8')
     const dom = new JSDOM(res)
@@ -260,7 +173,7 @@ async function getPublishTime(fileUrl) {
     console.log(publishTime); // 输出: 七刀社-全员吹
 }
 
-getViewText(fileUrl)
+// getViewText(fileUrl)
 async function getViewText(fileUrl) {
     const res = await fs.readFileSync(fileUrl, 'utf-8')
     const dom = new JSDOM(res)
@@ -278,3 +191,125 @@ async function getViewText(fileUrl) {
     console.log(viewTexts)
 
 }
+
+// main();
+
+async function main() {
+    const BVId = 'BV1nF411C7Rm';
+    const dirPath = path.resolve(__dirname, '../saveFile');
+    await createDirectory(dirPath);
+
+    const videoFileUrl = path.resolve(__dirname, `../saveFile/${BVId}.mp4`);
+    const audioFileUrl = path.resolve(__dirname, `../saveFile/${BVId}.mp3`);
+    const url = `https://www.bilibili.com/video/${BVId}/?spm_id_from=333.1007.search-card.all.click`
+    try {
+        console.log("视频网页信息请求中......");
+        const res = await axios({
+            method: 'get',
+            url,
+        })
+        console.log("视频网页信息请求完毕");
+        // const dirPath = path.resolve(__dirname, '../saveFile');
+        // await fs.writeFileSync(path.resolve(dirPath, `${BVId}.html`), res.data);
+
+        console.log('1///////////////////////////////////////////////////////')
+
+        console.log('视频相关信息（标题、作者、发布日期、观看量、关键字标签）获取中......')
+        const dom = new JSDOM(res.data)
+        const document = dom.window.document;
+        const title = document.title;
+        console.log('title:',title);
+
+        const authorTag = document.querySelector('meta[data-vue-meta="true"][itemprop="author"][name="author"]');
+        const author = authorTag ? authorTag.getAttribute('content') : null;
+        console.log('author:',author);
+
+        const timeTag = document.querySelector('meta[data-vue-meta="true"][itemprop="datePublished"]');
+        const publishTime = timeTag ? timeTag.getAttribute('content') : null;
+        console.log('publishTime:',publishTime);
+
+        const viewTextNodes = document.querySelectorAll('.view-text');
+        const viewTexts = [];
+        viewTextNodes.forEach(viewText => {
+            const text = viewText.textContent;
+            viewTexts.push(text);
+        });
+        console.log('viewTexts:',viewTexts);
+
+        const tagLinks = document.querySelectorAll('.tag-link');
+        const tags = [];
+        tagLinks.forEach(link => {
+            const text = link.textContent;
+            tags.push(text);
+        });
+        console.log('tags:',tags);
+
+        console.log('视频相关信息（标题、作者、发布日期、观看量、关键字标签）获取完毕')
+
+
+
+        console.log('2///////////////////////////////////////////////////////')
+        console.log("视频地址解析中......");
+        const scripts = document.querySelectorAll("script");
+        const playInfoString = scripts[3].textContent.replace('window.__playinfo__=', '');
+        const playInfo=JSON.parse(playInfoString)
+        const videoUrl = playInfo.data.dash.video[0].baseUrl;
+        console.log("视频地址解析完毕");
+        console.log("视频内容请求中......");
+        const videoData = await axios({
+            url:videoUrl,
+            responseType: 'arraybuffer',
+            method: 'get',
+            headers: {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+                "Referer": "https://search.bilibili.com/all?keyword=%E4%B8%80%E4%B8%AA%E8%B6%85%E7%9F%AD%E7%9A%84%E8%A7%86%E9%A2%91&from_source=webtop_search&spm_id_from=333.1007&search_source=5",
+            }
+        })
+        console.log("视频内容请求完毕");
+        console.log("视频文件保存中......");
+        fs.writeFileSync(videoFileUrl, videoData.data);
+        console.log("视频内容保存完毕");
+
+
+
+        console.log('3///////////////////////////////////////////////////////')
+        console.log("音频地址解析中......");
+        const audioUrl = playInfo.data.dash.audio[0].baseUrl;
+        console.log("音频地址解析完毕");
+        console.log("音频内容请求中......");
+        const audioData = await axios({
+            url:audioUrl,
+            responseType: 'arraybuffer',
+            method: 'get',
+            headers: {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+                "Referer": "https://search.bilibili.com/all?keyword=%E4%B8%80%E4%B8%AA%E8%B6%85%E7%9F%AD%E7%9A%84%E8%A7%86%E9%A2%91&from_source=webtop_search&spm_id_from=333.1007&search_source=5",
+            }
+        })
+        console.log("音频内容请求完毕");
+        console.log("音频文件保存中......");
+        fs.writeFileSync(audioFileUrl, audioData.data);
+        console.log("音频内容保存完毕");
+    } catch (err) {
+        return err;
+    }
+}
+
+
+async function createDirectory(dirPath) {
+    fs.mkdir(dirPath, {recursive: true}, (err) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                // 文件夹已存在
+                console.log(`The directory already exists: ${dirPath}`);
+            } else {
+                // 其他错误
+                console.error(`Error creating directory: ${err.message}`);
+            }
+        } else {
+            // 文件夹成功创建
+            console.log(`Directory created successfully: ${dirPath}`);
+        }
+    });
+}
+
