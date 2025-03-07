5acca7fcd6801721b6e86e981f5b7fb2fa9355a2
hugh <2401874452@qq.com>
2025-03-07 18:18:03 +0800

    更新代码

--- ai-use/src/store/useUserStore.ts
+++ ai-use/src/store/useUserStore.ts
@@ -1,5 +1,5 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import {defineStore} from "pinia";
+import {ref} from "vue";
 
 export const useUserStore = defineStore("UserStore", () => {
     const user = ref(null);
@@ -24,10 +24,36 @@ export const useUserStore = defineStore("UserStore", () => {
         localStorage.removeItem("user");
     };
 
+    const token = ref("");
+
+    const getToken = () => {
+        const storedUser = localStorage.getItem("token");
+        token.value = storedUser ? JSON.parse(storedUser) : null;
+        return token.value;
+    };
+
+    const setToken = (data) => {
+        if (data) {
+            token.value = data;
+            localStorage.setItem("token", JSON.stringify(token.value));
+        } else {
+            console.error("Invalid token data");
+        }
+    };
+
+    const removeToken = () => {
+        token.value = "";
+        localStorage.removeItem("token");
+    };
+
     return {
         user, // 暴露 user，以便外部可以直接访问
         getUser,
         setUser,
         removeUser,
+        token,
+        getToken,
+        setToken,
+        removeToken,
     };
 });
