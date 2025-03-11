8a3c5ca16b7718268730dc78412776dfaceee942
hugh <2401874452@qq.com>
2025-03-11 18:18:31 +0800

    增加Ai生成、批量添加节点功能的api

--- ai-use/src/api/index.ts
+++ ai-use/src/api/index.ts
@@ -1,5 +1,5 @@
 import request from "@/utils/request";
-import {Node_modify_options} from "@/api/interfance"
+import {Node_add_options, Ai_options, Node_modify_options} from "@/api/interfance"
 
 export const login = (data) => {
     return request({
@@ -17,7 +17,7 @@ export const node_list = (data) => {
     })
 }
 
-export const node_add = (data) => {
+export const node_add = (data: Node_add_options) => {
     return request({
         url: `node/add`,
         method: 'post',
@@ -34,10 +34,29 @@ export const node_delete = (node_id) => {
     })
 }
 
-export const node_modify = ((data:Node_modify_options) => {
+export const node_modify = ((data: Node_modify_options) => {
     return request({
         url: `node/modify`,
         method: 'post',
         data
     })
 })
+
+export const ai = ((data: Ai_options) => {
+    const {type_id, node_id} = data
+    if (!type_id || !node_id) return
+    return request({
+        url: `ai?type_id=${type_id}&node_id=${node_id}`,
+        method: 'get',
+    })
+})
+
+export const node_adds = (data: Node_add_options[]) => {
+    return request({
+        url: `node/adds`,
+        method: 'post',
+        data: {
+            node_list:data
+        }
+    })
+}
