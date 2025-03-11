ab7a1027e13b7d5b6426925c83811d7b543b7de4
hugh <2401874452@qq.com>
2025-03-11 18:16:28 +0800

    增加api注释

--- ai-use/src/api/interfance.ts
+++ ai-use/src/api/interfance.ts
@@ -1,5 +1,18 @@
+export interface Node_add_options {
+    project_id: number;
+    father_id: number;
+    type_id: number;
+    name: string;
+    abs: string;
+}
+
 export interface Node_modify_options {
-    node_id:number;
-    name:string;
-    abs:string;
+    node_id: number;
+    name: string;
+    abs: string;
+}
+
+export interface Ai_options {
+    type_id: number;
+    node_id: number;
 }
