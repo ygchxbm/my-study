3b83f11beab598f09ac5c9db91e0efbf881d0c03
hugh <2401874452@qq.com>
2025-03-17 18:14:18 +0800

    删除测试功能，优化drawer的效果、节点插入功能、右键菜单

--- ai-use/src/pages/Home.vue
+++ ai-use/src/pages/Home.vue
@@ -1,302 +1,116 @@
 <script setup lang="ts">
-import {ref, onMounted, computed, shallowRef} from 'vue'
+import {computed, h, onMounted, ref, shallowRef, watch} from 'vue'
 import MindMap from "simple-mind-map"
 import {createUid} from 'simple-mind-map/src/utils'
-import {ContextMenu, ContextMenuGroup, ContextMenuSeparator, ContextMenuItem} from '@imengyu/vue3-context-menu'
-import {ElDrawer} from 'element-plus'
+import {ContextMenu} from '@imengyu/vue3-context-menu'
+import {ElDrawer, ElMessage} from 'element-plus'
 import {Close, Discount} from '@element-plus/icons-vue'
 import Drawer from "@/components/Drawer.vue"
-import {node_add, node_list, node_delete, node_modify, node_adds} from "@/api"
-import RichText from 'simple-mind-map/src/plugins/RichText.js'
-import _ from "lodash"
+import {node_add, node_adds, node_delete, node_modify} from "@/api"
+import aiImage from "@/assets/image/AI.png"
+import '@/assets/iconfont.js'
+import {Node_item} from "@/api/interfance";
+import {storeToRefs} from "pinia"
+import {useNodeListStore} from "@/store/useNodeListStore";
 
+const {updateNodeList} = useNodeListStore();
+const {checkedNode} = storeToRefs(useNodeListStore());
 const mindMapContainer = ref()
-let mindMap = null
+let mindMap = null;
+
+watch(checkedNode, () => {
+  let data: MindNodeData = {}
+  if (checkedNode.value) {
+    data = parseNode(checkedNode.value)
+  }
+  mindMap.updateData(data);
+})
+
 
 // 当前激活的节点列表
 const activeNodes = shallowRef([])
-const activeNodeId = computed(() => {
+
+const lastActiveNode = ref({});
+const oneActiveResNodeData = computed(() => {
   if (activeNodes.value?.length > 0) {
-    return getId(activeNodes.value[0].uid)
+    const uid = activeNodes.value[0].uid;
+    return getResNode(uid);
   } else {
-    return undefined
+    return {}
   }
 })
 
-const activeNodeText = computed(() => {
-  if (activeNodes.value?.length > 0) {
-    return activeNodes.value[0].nodeData.data.text
-  } else {
-    return ''
+interface MindValueData {
+  uid: string;
+  text: string;
+  note: string;
+}
+
+interface MindNodeData {
+  data: MindValueData;
+  children?: MindNodeData[];
+}
+
+//插入子节点
+const insertChildNode = async (openEdit, fatherNodeList?: number[], appointNodeData: MindValueData[]) => {
+  if (fatherNodeList.length === 0 && activeNodes.value.length === 0) {
+    ElMessage.warning('请选择子节点插入的位置')
   }
-})
+  if (fatherNodeList.length === 0) {
+    fatherNodeList = activeNodes.value
+  }
 
-// 记录前进回退
-const isStart = ref(true)
-const isEnd = ref(true)
+  const {uid, text, note} = appointNodeData;
+  for (const fatherNode of fatherNodeList) {
+    const father_id = getResNode(fatherNode.uid).id;
+    if (!father_id || !fatherNode) continue
+    try {
+      const sort_index = fatherNode.getChildrenLength() + 1;
+      const res = await node_add({project_id: 1, father_id, "type_id": 1, sort_index, name: text, abs: note});
+      const {id} = res.data
+      Reflect.set(UIdMap, id, uid);
+      Reflect.set(resNodeMap, uid, res.data);
+      mindMap.execCommand('INSERT_CHILD_NODE', openEdit, [fatherNode], appointNodeData)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
 
+//插入同级节点
+const insertNode = async (openEdit, brotherNodeList?: number[], appointNodeData: MindNodeData[]) => {
+  if (brotherNodeList.length === 0 && activeNodes.value.length === 0) {
+    ElMessage.warning('请选择同级节点插入的位置')
+  }
+  if (brotherNodeList.length === 0) {
+    brotherNodeList = activeNodes.value;
+  }
 
-const buttons = ref([
-  {
-    label: '插入图片',
-    handler: () => {
-      activeNodes.value.forEach((node) => {
-        node.setImage({
-          url: 'https://lxqnsys.oss-cn-beijing.aliyuncs.com/qlx/xh2AXkBxYm5jGe5fD7DWYrC5b.png',
-          title: '图片的标题或描述',
-          width: 100,// 图片的宽高也不能少
-          height: 100
-        })
-      })
+  const {uid, text, note} = appointNodeData;
+  for (const brotherNode of brotherNodeList) {
+    const fatherNode = brotherNode.parent;
+    if (!fatherNode) {
+      ElMessage.warning("添加同级节点失败，只能拥有一个根节点");
+      continue;
     }
-  },
-  {
-    label: '插入图标',
-    handler: () => {
-      const iconList = ['priority_1', 'priority_2']
-      activeNodes.value.forEach(node => {
-        node.setIcon(iconList)
-      })
+    const father_id = getResNode(fatherNode.uid).id;
+    if (!father_id) continue
+    try {
+      const sort_index = fatherNode.getChildrenLength();
+      const res = await node_add({project_id: 1, father_id, "type_id": 1, sort_index, name: text, abs: note});
+      const {id} = res.data
+      Reflect.set(UIdMap, uid, id);
+      Reflect.set(resNodeMap, uid, res.data);
+      mindMap.execCommand('INSERT_NODE', openEdit, [brotherNode], appointNodeData)
+    } catch (e) {
+      console.error(e)
     }
-  },
-  {
-    label: '插入超链接',
-    handler: () => {
-      activeNodes.value.forEach(node => {
-        node.setHyperlink('http://lxqnsys.com/', '理想青年实验室')
-      })
-    }
-  },
-  {
-    label: '插入备注',
-    handler: () => {
-      activeNodes.value.forEach(node => {
-        node.setNote('备注内容')
-      })
-    }
-  },
-  {
-    label: '插入标签',
-    handler: () => {
-      activeNodes.value.forEach(node => {
-        node.setTag(['标签1', '标签2'])
-      })
-    }
-  },
-  {
-    label: '插入概要',
-    handler: () => {
-      mindMap.execCommand('ADD_GENERALIZATION', {
-        text: '自定义概要内容'
-      })
-    }
-  },
-  {
-    label: '插入关联线',
-    handler: () => {
-      mindMap.associativeLine.createLineFromActiveNode()
-    }
-  },
-  {
-    label: '回退',
-    handler: () => {
-      mindMap.execCommand('BACK')
-    }
-  },
-  {
-    label: '前进',
-    handler: () => {
-      mindMap.execCommand('FORWARD')
-    }
-  },
-  {
-    label: '插入兄弟节点',
-    handler: () => {
-      mindMap.execCommand('INSERT_NODE')
-    }
-  },
-  {
-    label: '插入子节点',
-    handler: () => {
-      mindMap.execCommand('INSERT_CHILD_NODE')
-    }
-  },
-  {
-    label: '删除节点',
-    handler: () => {
-      mindMap.execCommand('REMOVE_NODE')
-    }
-  },
-  {
-    label: '设置背景颜色',
-    handler: () => {
-      mindMap.setThemeConfig({
-        backgroundColor: '#eeeff1'
-      })
-    }
-  },
-])
+  }
+
+}
 
 const drawerType = ref(1)
 
-const menuData = ref([
-  {
-    label: 'AI用例衍生',
-    type: 1,//item
-    handler: () => {
-      drawerVisible.value = true
-    },
-  },
-  {
-    label: 'AI生成',
-    type: 1,//item
-    handler: () => {
-      drawerVisible.value = true
-      drawerType.value = 2
-    },
-  },
-  {
-    type: 3,//分割线
-  },
-  {
-    label: '新增子节点',
-    type: 1,//item
-    handler: async () => {
-      if (activeNodes.value.length === 0) return
-      const father_id = getId(activeNodes.value[0]?.uid);
-      if (!father_id) return
-      const name = "新节点";
-      const abs = "新备注";
-      const res = await node_add({"project_id": 1, father_id, "type_id": 1, name, abs});
-      if (!res.data) return
-      const {id} = res.data
-      const uid = createUid();
-      Reflect.set(UIdMap, uid, id);
-      console.log(uid)
-      mindMap.execCommand('INSERT_CHILD_NODE', false, [], {
-        uid,
-        text: name,
-        note: abs
-      })
-    },
-  },
-  {
-    label: '新增邻节点',
-    type: 1,//item
-    handler: async () => {
-      if (activeNodes.value.length === 0) return
-
-      const {parent} = activeNodes.value[0]
-      if (!parent) return
-      const father_id = getId(parent?.uid);
-      if (!father_id) return
-      const name = "新节点";
-      const abs = "新备注";
-      const res = await node_add({"project_id": 1, father_id, "type_id": 1, name, abs});
-      if (!res.data) return
-      const {id} = res.data
-      const uid = createUid();
-      Reflect.set(UIdMap, uid, id);
-      mindMap.execCommand('INSERT_NODE', false, [], {
-        uid,
-        text: name,
-        note: abs
-      })
-    },
-  },
-  {
-    label: '收起',
-    type: 2,//group
-  },
-  {
-    label: '插入',
-    type: 2,
-    children: [
-      {
-        label: '图片',
-        type: 1,//item
-        handler: () => {
-          activeNodes.value.forEach((node) => {
-            node.setImage({
-              url: 'https://lxqnsys.oss-cn-beijing.aliyuncs.com/qlx/xh2AXkBxYm5jGe5fD7DWYrC5b.png',
-              title: '图片的标题或描述',
-              width: 100,// 图片的宽高也不能少
-              height: 100
-            })
-          })
-        }
-      },
-      {
-        label: '图标',
-        type: 1,//item
-        handler: () => {
-          const iconList = ['priority_1', 'priority_2']
-          activeNodes.value.forEach(node => {
-            node.setIcon(iconList)
-          })
-        }
-      },
-      {
-        label: '备注',
-        type: 1,//item
-        handler: () => {
-          activeNodes.value.forEach(node => {
-            node.setNote('备注内容')
-          })
-        }
-      },
-      {
-        label: '标签',
-        type: 1,//item
-        handler: () => {
-          activeNodes.value.forEach(node => {
-            node.setTag(['标签1', '标签2'])
-          })
-        }
-      },
-      {
-        label: '超链接',
-        type: 1,//item
-        handler: () => {
-          activeNodes.value.forEach(node => {
-            node.setHyperlink('http://lxqnsys.com/', '理想青年实验室')
-          })
-        }
-      },
-      {
-        label: '概要',
-        type: 1,//item
-        handler: () => {
-          mindMap.execCommand('ADD_GENERALIZATION', {
-            text: '自定义概要内容'
-          })
-        }
-      },
-      {
-        label: '关联线',
-        type: 1,//item
-        handler: () => {
-          mindMap.associativeLine.createLineFromActiveNode()
-        }
-      },
-    ]
-  },
-  {
-    label: '删除节点',
-    type: 1,
-    handler: async () => {
-      if (activeNodes.value.length > 0) {
-        const {uid} = activeNodes.value[0];
-        const id = getId(uid);
-        if (!id || id === 0) return
-        const res = await node_delete(id)
-        if (res.data) {
-          mindMap.execCommand('REMOVE_NODE')
-        }
-      }
-    },
-  },
-])
 
 // 当前右键点击的类型
 const type = ref('')
@@ -312,12 +126,112 @@ const show = ref(false)
 const optionsComponent = computed(() => {
   return {
     zIndex: 3,
-    minWidth: 230,
+    minWidth: 100,
     x: left.value,
     y: top.value,
+    items: [
+      // {
+      //   label: h('div', {
+      //     style: {
+      //       fontSize: '14px',
+      //       background: 'linear-gradient(45deg, #ff0000, #ff00b9, #0067ff)',
+      //       '-webkit-background-clip': 'text',
+      //       color: 'transparent',
+      //     }
+      //   }, 'AI用例衍生'),
+      //   icon: h('img', {
+      //     src: aiImage,
+      //     style: {
+      //       width: '16px',
+      //       height: '16px',
+      //     }
+      //   }),
+      //   onClick: () => {
+      //     drawerVisible.value = true
+      //   }
+      // },
+      {
+        label: h('div', {
+          style: {
+            fontSize: '14px',
+            background: 'linear-gradient(45deg, #ff0000, #ff00b9, #0067ff)',
+            '-webkit-background-clip': 'text',
+            color: 'transparent',
+          }
+        }, 'AI生成'),
+        icon: h('img', {
+          src: aiImage,
+          style: {
+            width: '16px',
+            height: '16px',
+          }
+        }),
+        onClick: () => {
+          drawerVisible.value = true
+          drawerType.value = 2
+        },
+        divided: 'down'
+      },
+      {
+        label: '新增子节点',
+        svgIcon: '#icon-zhuye',
+        onClick: () => {
+          insertChildNode(true, [], {
+            uid: createUid(),
+            text: '新节点',
+            note: '备注内容'
+          })
+        }
+      },
+      {
+        label: '新增同级节点',
+        svgIcon: '#icon-zhuye',
+        onClick: async () => {
+          await insertNode(true, [], {
+            uid: createUid(),
+            text: '新节点',
+            note: '备注内容'
+          })
+        }
+      },
+      // {
+      //   label: '插入',
+      //   svgIcon: '#icon-xinzeng',
+      //   children: [
+      //     {
+      //       label: '图片',
+      //     },
+      //   ]
+      // },
+      // {
+      //   label: '标记节点类型',
+      //   svgIcon: '#icon-biaozhu',
+      // },
+      {
+        label: h('div', {
+          style: {
+            fontSize: '14px',
+            color: 'red',
+          }
+        }, '删除节点'),
+        divided: 'up',
+        svgIcon: '#icon-shanchu',
+
+        onClick: async () => {
+          if (activeNodes.value.length > 0) {
+            const {uid} = activeNodes.value[0];
+            const id = getResNode(uid).id;
+            if (!id || id === 0) return
+            const res = await node_delete(id)
+            if (res.data) {
+              mindMap.execCommand('REMOVE_NODE')
+            }
+          }
+        }
+      },
+    ]
   }
 })
-
 
 // 记录鼠标右键按下的位置
 const mousedownX = ref(0)
@@ -325,38 +239,23 @@ const mousedownY = ref(0)
 const isMousedown = ref(false)
 
 
-const UIdMap = {}
-const getId = (uid) => {
-  if (uid) {
-    return UIdMap[uid]
-  }
-}
-
 onMounted(async () => {
-  const nodeList = await node_list({"project_id": 1, "father_id": 0, "type_id": 1})
-  // await node_delete(4)
-  if (nodeList.data.length < 1) return
-  const data = parseNode(nodeList.data[0])
-
   mindMap = new MindMap({
     el: mindMapContainer.value,
-    data,
-    initRootNodePosition: ['left', 'center'],
+    data: {},
+    initRootNodePosition: ['20%', '50%'],
     richText: true
   });
-
-  MindMap.usePlugin(RichText, {})
+  mindMap.setThemeConfig({
+    backgroundColor: '#f7f7f7'
+  })
 
   // 监听节点激活事件
   mindMap.on('node_active', (node, nodeList) => {
     activeNodes.value = nodeList
+    lastActiveNode.value = nodeList[nodeList.length - 1]
   })
 
-  // 前进回退事件
-  mindMap.on('back_forward', (index, len) => {
-    isStart.value = index <= 0
-    isEnd.value = index >= len - 1
-  })
 
   //右键菜单
   const hide = () => {
@@ -412,106 +311,120 @@ onMounted(async () => {
   // 监听节点内容变化
   mindMap.on('data_change_detail', (arr) => {
     arr.forEach(async item => {
-      if (item?.oldData?.data && !_.isEqual(item.data.data, item.oldData.data)) {
-        //文本变化
-        const {text, note, uid} = item.data.data;
-        const node_id = getId(uid);
-        console.log({text, note, node_id})
-        if (!node_id) return
-        await node_modify({node_id, name: text, abs: note});
+      if (item?.oldData?.data) {
+        let isEqual = true;
+        ['text', 'note', 'uid'].forEach(key => {
+          if (item.data.data[key] !== item.oldData.data[key]) {
+            isEqual = false;
+          }
+        })
+        if (!isEqual) {
+          //文本变化
+          const {text, note, uid} = item.data.data;
+          const resNode = getResNode(uid);
+          const {id, sort_index} = resNode
+          const name1 = extractSimplifiedContentAndText(text)
+          if (!id || typeof sort_index !== 'number' || isNaN(sort_index)) return
+          await node_modify({node_id: id, sort_index, name: name1, abs: note});
+        }
       }
     });
   });
 })
 
-
 //Drawer抽屉
 const drawerVisible = ref(false)
 
-const addNodes = async (type_id: number, texts: string[]) => {
-  console.log(type_id)
-  console.log(texts)
-  const obj = []
-  const father_id = activeNodes.value?.length > 0 ? getId(activeNodes.value[0].uid) : ''
-  texts.forEach(text => {
-    obj.push({
-      project_id: 1,
-      father_id,
-      type_id: type_id,
-      name: text,
-      abs: '',
-    })
-  })
+interface EmitNodeList {
+  data: {
+    text: string;
+    type_id: number;
+  }
+  children: EmitNodeList[]
+}
 
-  const res = await node_adds(obj)
-  if (res?.data?.length === 0) return
-  const {data} = res;
-  const childList = []
-  data.forEach(({id, name, abs}) => {
-    const uid = createUid();
-    Reflect.set(UIdMap, uid, id);
-    childList.push({
-      data: {
-        uid,
-        text: name,
-        note: abs
+const addNodes = async (father_id, emitNodeList: EmitNodeList[]) => {
+  const parseRes = (emitNodeList: EmitNodeList[]) => {
+    const arr = [];
+    emitNodeList.forEach(({data, children}, index) => {
+      const {text, type_id} = data;
+      const obj = {
+        project_id: 1,
+        father_id,
+        type_id,
+        sort_index: index + 1,
+        name: text,
+        abs: '',
+        children: []
+      }
+      arr.push(obj)
+      if (children?.length > 0) {
+        obj.children.push(...parseRes(children))
       }
     })
-  })
 
-  mindMap.execCommand('INSERT_MULTI_CHILD_NODE', [], childList)
+    return arr
+  }
+
+  const arr = parseRes(emitNodeList)
+  try {
+    await node_adds(arr)
+    await updateNodeList()
+  } catch (err) {
+
+  }
+
   drawerVisible.value = false;
 }
 
-const parseNode = (node) => {
-  const data = {}
+const parseNode = (node: Node_item) => {
+  node.children.sort((a, b) => {
+    return a.sort_index - b.sort_index
+  })
+  const data: MindNodeData = {}
   const {id, name, abs, children} = node
   const uid = createUid();
-  Reflect.set(UIdMap, uid, id);
+  Reflect.set(UIdMap, id, uid);
+  Reflect.set(resNodeMap, uid, node);
   Reflect.set(data, "data", {text: name, uid, note: abs})
   Reflect.set(data, "children", [])
   if (children) {
     for (const item of children) {
-      data.children.unshift(parseNode(item))
+      data.children.push(parseNode(item))
     }
   }
   return data
 }
 
+const extractSimplifiedContentAndText = (str) => {
+  const regex = /<\/?p>/g;
+  return str?.replace(regex, '');
+}
+
+const UIdMap = {}
+const resNodeMap = {}
+const getResNode = (uid) => {
+  if (uid) {
+    return Reflect.get(resNodeMap, uid)
+  }
+}
+
+
 </script>
 
 <template>
   <div class="flex flex-col h-full">
-    <div class="toolbar flex gap-2 p-2 items-center flex-wrap">
-      <button
-          v-for="(button, index) in buttons"
-          :key="index"
-          @click="button.handler"
-          class="h-10 px-4 bg-amber-200 rounded"
-      >
-        {{ button.label }}
-      </button>
-    </div>
     <div ref="mindMapContainer" class="w-full h-full"></div>
+
     <context-menu
         v-model:show="show"
         :options="optionsComponent">
-      <template v-for="item in menuData">
-        <context-menu-item v-if="item?.type===1" :label="item.label" @click="item.handler"></context-menu-item>
-        <context-menu-group v-if="item?.type===2" :label="item.label">
-          <template v-for="i in item.children">
-            <context-menu-item v-if="i?.type===1" :label="i.label" @click="i.handler"></context-menu-item>
-            <context-menu-separator v-if="i?.type===3"/>
-          </template>
-        </context-menu-group>
-        <context-menu-separator v-if="item?.type===3"/>
-      </template>
     </context-menu>
     <el-drawer v-model="drawerVisible" :show-close="false">
       <template #header="{ close, titleId }" class="p-0">
         <div class="flex items-baseline gap-2">
           <div class="border-1 px-1 border-green-400 text-green-400" style="font-size: 12px">模块</div>
-          <b><span>{{ activeNodes?.length > 0 ? activeNodes[0].nodeData.data.text : '' }}</span></b>
+          <b>{{ oneActiveResNodeData?.name }}</b>
           <div class="px-1 text-green-400" style="font-size: 12px">
             <el-icon>
               <Discount/>
@@ -527,8 +440,9 @@ const parseNode = (node) => {
           @addNodes="addNodes"
           :drawerType="drawerType"
           :drawerVisible="drawerVisible"
-          :node_id=activeNodeId
-          :node_text=activeNodeText
+          :node_id=oneActiveResNodeData?.id
+          :node_text=oneActiveResNodeData?.name
+          :node_type_id=oneActiveResNodeData?.type_id
       />
     </el-drawer>
   </div>
